97c27f0fd95a13d12d2ae7787158fc166c807f09
= <=>
2021-06-26 18:36:56 -0400

    Error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { signinRouter } from './routes/signin';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentUser', (req, res) => {
-    res.send('Hi there');
-});
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(signinRouter);
+app.use(errorHandler);
 
 app.listen(3000, ()=> {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000');
 });
---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+  ) => {
+  if(err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param };
+    });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  if(err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  } 
+  res.status(400).send({
+    errors: [{ message: 'Something went wrong' }]
+  });
+};
