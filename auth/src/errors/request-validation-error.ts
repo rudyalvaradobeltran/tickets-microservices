5eb73a6747200722752cec3c008369c34e14195c
= <=>
2021-06-26 20:41:31 -0400

    Async error handling

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,12 +1,11 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super();
-
-    // Only because we are extending a built in class
+    super('Invalid request parameters');
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
